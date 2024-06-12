025798ffd6453af4b8858789583036e5b2df48fd
aidanaden <aidan@u.nus.edu>
2024-06-13 00:51:03 +0800

    feat: tmux resurrect + continuum, updated sessionizier with coinhall/apps, coinhall/packages, nvim og config

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/projects ~/ ~/coinhall -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/projects ~/ ~/coinhall ~/coinhall/monoapp ~/coinhall/monoapp/apps ~/coinhall/monoapp/packages -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
