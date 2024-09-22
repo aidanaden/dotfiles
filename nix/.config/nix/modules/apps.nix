4393307385f30f8ee05607b098d8e076283726c6
aidanaden <aidan@u.nus.edu>
2024-09-23 04:17:54 +0800

    test: hello

--- nix/.config/nix/modules/apps.nix
+++ nix/.config/nix/modules/apps.nix
@@ -119,7 +119,14 @@
     # pkgs.callPackage
     # ./figurine.nix
     # {}
-    (import ./my-hello.nix)
+    (stdenv.mkDerivation
+      rec {
+        name = "hello-2.8";
+        src = fetchurl {
+          url = "mirror://gnu/hello/${name}.tar.gz";
+          sha256 = "0wqd8sjmxfskrflaxywc7gqw7sfawrfvdxd9skxawzfgyy0pzdz6";
+        };
+      })
   ];
 
   launchd = {
