2229f53030d73b69ff9ff869fba44e2c6a13482f
aidanaden <aidan@u.nus.edu>
2024-10-05 23:10:32 +0800

    nixos: remove zsh

--- nix/.config/nix/nixos/default.nix
+++ nix/.config/nix/nixos/default.nix
@@ -23,8 +23,4 @@
       enable = true;
     };
   };
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # this is required if you want to use darwin's default shell - zsh
-  programs.zsh.enable = true;
 }
