23e998cf1735bc0f54b6527fa3775368aa71371f
aidanaden <aidan@u.nus.edu>
2024-10-07 06:32:36 +0800

    test: enable display manager 4

--- nix/.config/nix/nixos/default.nix
+++ nix/.config/nix/nixos/default.nix
@@ -75,15 +75,15 @@ in {
   # };
 
   services = {
-    # xserver = {
-    #   displayManager.sddm.wayland.enable = true;
-    #   enable = true;
-    #   #   desktopManager.gnome.enable = true;
-    #   xkb = {
-    #     layout = "us";
-    #     variant = "";
-    #   };
-    # };
+    xserver = {
+      displayManager.sddm.wayland.enable = true;
+      enable = true;
+      #   desktopManager.gnome.enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
 
     # Enable fingerprint
     fprintd = {
