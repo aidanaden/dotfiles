4fd9fa29332560f5c86dd62108e8ef72d7fa52e9
aidanaden <aidan@u.nus.edu>
2024-10-07 06:25:07 +0800

    feat: nixos hyprland

--- nix/.config/nix/home/wayland.nix
+++ nix/.config/nix/home/wayland.nix
@@ -1,14 +1,15 @@
 {...}: {
-  wayland.windowManager.sway = {
-    enable = true;
-    config = rec {
-      modifier = "Mod4"; # Super key
-      terminal = "kitty";
-      output = {
-        "Virtual-1" = {
-          mode = "2560x1440@60Hz";
-        };
-      };
-    };
-  };
+  programs.hyprland.enable = true; # enable Hyprland
+  # wayland.windowManager.sway = {
+  #   enable = true;
+  #   config = rec {
+  #     modifier = "Mod4"; # Super key
+  #     terminal = "kitty";
+  #     output = {
+  #       "Virtual-1" = {
+  #         mode = "2560x1440@60Hz";
+  #       };
+  #     };
+  #   };
+  # };
 }
