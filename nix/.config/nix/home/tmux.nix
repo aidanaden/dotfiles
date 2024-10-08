eef3dd239a0084acf0728bc1c526bfbe27c60ed9
aidanaden <aidan@u.nus.edu>
2024-10-08 17:45:29 +0800

    fix: unstable tmux tokyo-night

--- nix/.config/nix/home/tmux.nix
+++ nix/.config/nix/home/tmux.nix
@@ -1,9 +1,11 @@
 {
-  config,
   pkgs,
   lib,
+  inputs,
   ...
-}: {
+}: let
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+in {
   programs.tmux = {
     enable = true;
     shortcut = "b";
@@ -38,28 +40,28 @@
           set -g @session-wizard-mode "full-path"
         '';
       }
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor 'mocha' # latte, frappe, macchiato or mocha
-        '';
-      }
       # {
-      #   plugin = tmuxPlugins.tmuxplugin-tokyo-night-tmux;
+      #   plugin = unstable.tmuxPlugins.catppuccin;
       #   extraConfig = ''
-      #     set -g @tokyo-night-tmux_window_id_style digital
-      #     set -g @tokyo-night-tmux_pane_id_style hsquare
-      #     set -g @tokyo-night-tmux_zoom_id_style dsquare
-      #
-      #     set -g @tokyo-night-tmux_show_path 1
-      #     set -g @tokyo-night-tmux_path_format relative # 'relative' or 'full'
-      #
-      #     set -g @tokyo-night-tmux_show_netspeed 1
-      #     set -g @tokyo-night-tmux_netspeed_iface "en0" # Detected via default route
-      #     set -g @tokyo-night-tmux_netspeed_showip 1      # Display IPv4 address (default 0)
-      #     set -g @tokyo-night-tmux_netspeed_refresh 1     # Update interval in seconds (default 1)
+      #     set -g @catppuccin_flavor 'mocha' # latte, frappe, macchiato or mocha
       #   '';
       # }
+      {
+        plugin = unstable.tmuxPlugins.tokyo-night-tmux;
+        extraConfig = ''
+          set -g @tokyo-night-tmux_window_id_style digital
+          set -g @tokyo-night-tmux_pane_id_style hsquare
+          set -g @tokyo-night-tmux_zoom_id_style dsquare
+
+          set -g @tokyo-night-tmux_show_path 1
+          set -g @tokyo-night-tmux_path_format relative # 'relative' or 'full'
+
+          set -g @tokyo-night-tmux_show_netspeed 1
+          set -g @tokyo-night-tmux_netspeed_iface "en0" # Detected via default route
+          set -g @tokyo-night-tmux_netspeed_showip 1      # Display IPv4 address (default 0)
+          set -g @tokyo-night-tmux_netspeed_refresh 1     # Update interval in seconds (default 1)
+        '';
+      }
     ];
 
     extraConfig = lib.strings.fileContents ./tmux.conf;
