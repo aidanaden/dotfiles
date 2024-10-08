ff5cd202956175fb2d508e7250609275a9fe402a
aidanaden <aidan@u.nus.edu>
2024-10-09 00:05:22 +0800

    fix: wayland config 2

--- nix/.config/nix/home/wayland.nix
+++ nix/.config/nix/home/wayland.nix
@@ -54,8 +54,8 @@ in {
         gaps_out = 6;
         border_size = 2;
         layout = "dwindle";
-        col.active_border = "rgba(7aa2f7aa)";
-        col.inactive_border = "rgba(414868aa)";
+        "col.active_border" = "rgba(7aa2f7aa)";
+        "col.inactive_border" = "rgba(414868aa)";
         allow_tearing = true;
       };
 
