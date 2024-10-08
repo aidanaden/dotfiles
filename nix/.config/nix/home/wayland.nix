fd760b05d717796ddedf1cead2f433ba6f43f8fe
aidanaden <aidan@u.nus.edu>
2024-10-09 00:17:35 +0800

    test: cursor size

--- nix/.config/nix/home/wayland.nix
+++ nix/.config/nix/home/wayland.nix
@@ -214,12 +214,14 @@ in {
         "XDG_SESSION_TYPE,wayland"
         "XDG_SESSION_DESKTOP,Hyprland"
         "XDG_CURRENT_DESKTOP,Hyprland"
+        "HYPRCURSOR_SIZE,24"
+        "XCURSOR_SIZE,24"
       ];
 
       exec-once = [
         "${unstable.vesktop}/bin/vesktop"
-        "kitty"
         "firefox"
+        "kitty"
         "dunst"
         "swww init & sleep 0.5 && exec wallpaper_random"
         # "eval $(gnome-keyring-daemon --start --components=secrets,ssh,gpg,pkcs11)"
