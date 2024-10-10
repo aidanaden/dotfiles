95bf274e6136227b01f1608e527aca12924e33f5
aidanaden <aidan@u.nus.edu>
2024-10-10 23:01:43 +0800

    fix: home 2

--- nix/.config/nix/nixos/x1/flake.nix
+++ nix/.config/nix/nixos/x1/flake.nix
@@ -94,7 +94,7 @@
                 imports = [
                   inputs.spicetify-nix.homeManagerModules.default
                   catppuccin.homeManagerModules.catppuccin
-                  ../home/nixos.nix
+                  ../../home/nixos.nix
                 ];
                 home.stateVersion = "24.05";
               };
