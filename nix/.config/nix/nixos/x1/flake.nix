81d8027a2f57df5f3b21807f30cdf1fc4b49bcd5
aidanaden <aidan@u.nus.edu>
2024-10-10 23:10:41 +0800

    fix: self 2

--- nix/.config/nix/nixos/x1/flake.nix
+++ nix/.config/nix/nixos/x1/flake.nix
@@ -70,12 +70,13 @@
       # Makes all inputs availble in imported files
       specialArgs = {inherit inputs user hostname overlays nixpkgsConfig;};
 
-      system = {
-        stateVersion = "5";
-        configurationRevision = self.rev or self.dirtyRev or null;
-      };
-
       modules = [
+        ({...}: {
+          system = {
+            stateVersion = "5";
+            configurationRevision = self.rev or self.dirtyRev or null;
+          };
+        })
         catppuccin.nixosModules.catppuccin
         # Add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
         nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
