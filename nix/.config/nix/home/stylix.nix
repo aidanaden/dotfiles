f93b1dd04c9a206c5a4a0608d6a8c43d903377ad
aidanaden <aidan@u.nus.edu>
2024-10-15 02:34:49 +0800

    feat: wallpaper

--- nix/.config/nix/home/stylix.nix
+++ nix/.config/nix/home/stylix.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    autoEnable = true;
+    autoEnable = false;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
-    image = "";
+    image = ../wallpapers/tokyodark/wallpaper1.png;
 
     targets = {
       kitty = {
@@ -16,13 +16,26 @@
         enableLeftBackColors = false;
         enableRightBackColors = false;
       };
+      rofi = {
+        enable = true;
+      };
+      hyprland = {
+        enable = true;
+      };
+    };
+
+    opacity = {
+      desktop = 1.0;
+      popups = 1.0;
+      applications = 1.0;
+      terminal = 1.0;
     };
 
     fonts = {
       sizes = {
         desktop = 12;
         popups = 12;
-        terminal = 12;
+        terminal = 11;
         applications = 12;
       };
       serif = {
