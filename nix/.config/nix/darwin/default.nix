18526691dff90c35fa9e0c9addea27da1430805d
aidanaden <aidan@u.nus.edu>
2024-10-18 11:18:35 +0800

    fix: remove auto download

--- nix/.config/nix/darwin/default.nix
+++ nix/.config/nix/darwin/default.nix
@@ -191,7 +191,7 @@
           # Check for software updates daily, not just once per week
           ScheduleFrequency = 1;
           # Download newly available updates in background
-          AutomaticDownload = 1;
+          AutomaticDownload = 0;
           # Install System data files & security updates
           CriticalUpdateInstall = 1;
         };
