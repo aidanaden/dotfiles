cd4cb311ebf88d677b23d8256d1ea51c08fea100
aidanaden <aidan@u.nus.edu>
2024-11-04 02:44:48 +0800

    fix: unstable jj

--- nix/.config/nix/home/jujutsu.nix
+++ nix/.config/nix/home/jujutsu.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+in
 {
   programs.jujutsu = {
     enable = true;
+    package = unstable.jujutsu;
     settings = {
       name = "aidan";
       email = "aidanaden@hotmail.com";
